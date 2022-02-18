d8d52b7874d5d0b8ab557aea4bf3aac3a9397135
audreyduarte <audreyarduarte@recife.pe.senac.br>
2022-02-18 19:32:20 -0300

    projeto

---
+++ aula-js/for-in.js
@@ -0,0 +1,12 @@
+let pessoa = {
+ nome: "Audrey",
+ idade: "30",
+ sexo: "feminino",
+ peso: "depende"  
+}
+
+for (let p in pessoa){
+    console.log('dados da pessoa', p)
+    console.log('dados da pessoa', pessoa [p])
+}
+//
---
+++ aula-js/forsimples.js
@@ -0,0 +1,7 @@
+const notas = [10,5,6,7]
+
+console.log('oi')
+
+for (let i = 0; i< notas.length; i++) {
+    console.log('indice', i,'valor: ', notas[i])
+}
